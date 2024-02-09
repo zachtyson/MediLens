c4c51a14ddc159865873cce8daf60cdeb5e28c50
Zachary <tysonzachary@ufl.edu>
2024-02-08 23:58:45 -0500

    create backend base db package

---
+++ backend/db/session.py
@@ -0,0 +1,25 @@
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, MetaData
+from databases import Database
+from core.config import settings
+
+# Create the engine
+engine = create_engine(settings.DATABASE_URL, pool_size=5, max_overflow=1)
+
+# Create a session factory
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Create a databases.Database instance
+database = Database(settings.DATABASE_URL)
+
+# Define the metadata
+metadata = MetaData()
+
+
+def get_db() -> Session:
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
