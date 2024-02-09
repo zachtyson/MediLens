73aa57ea32737bc18c0430c1a178e6446758b5f2
chuerta018 <chuerta018@gmail.com>
2024-02-08 23:37:35 -0500

    NavController to between a " login screen" and a home page: to return back to "login screen" click on settings button. login screen is just a button lol

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -58,12 +58,22 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
+    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+    //
+    implementation("androidx.compose.material3:material3:1.2.0")
+    implementation("androidx.compose.material3:material3-window-size-class:1.2.0")
+    implementation("androidx.compose.material3:material3-adaptive:1.0.0-alpha06")
+    implementation("androidx.compose.material3:material3-adaptive-navigation-suite:1.0.0-alpha03")
+
+    val nav_version = "2.7.7"
+    implementation("androidx.navigation:navigation-compose:$nav_version")
+    //
 }
--- android-app/app/src/main/java/com/ztch/medilens_android_app/MainActivity.kt
+++ android-app/app/src/main/java/com/ztch/medilens_android_app/MainActivity.kt
@@ -3,44 +3,90 @@ package com.ztch.medilens_android_app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Settings
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.ztch.medilens_android_app.ui.theme.MedilensandroidappTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MedilensandroidappTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+
+                MyApp()
             }
         }
     }
 }
 
+// Define the Profile composable.
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Login(onNavigateToHome: () -> Unit) {
+
+    Button(onClick = { onNavigateToHome()}) {
+        Text("Go to HomePage")
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    MedilensandroidappTheme {
-        Greeting("Android")
+fun Home(onNavigateToLogin: () -> Unit, ) {
+
+    // Use LocalDensity to convert dp to pixels
+    val density = LocalDensity.current.density
+
+    // Set padding for the icon button
+    val iconButtonPadding = 16.dp
+
+    // Calculate padding in pixels
+    val iconButtonPaddingPx = with(LocalDensity.current) { iconButtonPadding.roundToPx() }
+
+    // Create a Compose Box
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(end = iconButtonPadding)
+            .padding(bottom = iconButtonPadding)
+
+    ) {
+        IconButton(
+            onClick = { onNavigateToLogin() },
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+        ) {
+            Icon(
+                Icons.Rounded.Settings,
+                contentDescription = "Settings",
+                tint = MaterialTheme.colorScheme.primary
+            )
+        }
+    }
+}
+
+
+
+// Define the MyApp composable, including the `NavController` and `NavHost`.
+@Composable
+fun MyApp() {
+    val navController = rememberNavController()
+    NavHost(navController, startDestination = "Login") {
+        composable("Login") { Login(onNavigateToHome = { navController.navigate("Home") }) }
+        composable("Home") { Home(onNavigateToLogin= { navController.navigate("Login") }) }
     }
 }
