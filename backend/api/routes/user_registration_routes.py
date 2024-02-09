39cf7ff0c27578fc967635a8d4afde12b612da72
Zachary <tysonzachary@ufl.edu>
2024-02-09 00:02:10 -0500

    backend api route for registering user

---
+++ backend/api/routes/user_registration_routes.py
@@ -0,0 +1,70 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from models.user import User
+from db.session import SessionLocal
+from schemas.user import UserCreate, UserUpdate, UserResponse
+from typing import List
+from core.security import get_password_hash
+
+router = APIRouter()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@router.get("/users", response_model=List[UserResponse])
+async def get_users(db: Session = Depends(get_db)):
+    # this definitely should not be in a production environment, so we will remove it later
+    users = db.query(User).all()
+    return users
+
+
+@router.get("/users/{user_id}", response_model=UserResponse)
+async def get_user(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.id == user_id).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
+
+
+@router.post("/users", response_model=UserResponse)
+async def create_user(user: UserCreate, db: Session = Depends(get_db)):
+
+    existing_user = db.query(User).filter(User.username == user.username).first()
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Username already registered")
+
+    existing_email = db.query(User).filter(User.email == user.email).first()
+    if existing_email:
+        raise HTTPException(status_code=400, detail="Email already registered")
+
+    user.password = get_password_hash(user.password)
+
+    db_user = User(username=user.username, email=user.email, hashed_password=user.password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+
+@router.put("/users/{user_id}", response_model=UserResponse)
+async def update_user(user_id: int, user: UserUpdate, db: Session = Depends(get_db)):
+    db_user = db.query(User).filter(User.id == user_id).first()
+    if not db_user:
+        raise HTTPException(status_code=404, detail="User not found")
+    if user.username:
+        db_user.username = user.username
+    if user.email:
+        db_user.email = user.email
+    if user.password:
+        db_user.password = user.password
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+
