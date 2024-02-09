b7d6314f0504e9bbe82b5cfcce99a68be112c172
Zachary <tysonzachary@ufl.edu>
2024-02-09 00:00:19 -0500

    create user backend model

---
+++ backend/models/user.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String, DateTime, func, ForeignKey, Table
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+metadata = Base.metadata
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)  # Use a larger integer data type if needed
+    username = Column(String(30), index=True, unique=True)
+    email = Column(String(100), index=True, unique=True)
+    hashed_password = Column(String(100))
+    created_date = Column(DateTime, default=func.now(), nullable=False)
