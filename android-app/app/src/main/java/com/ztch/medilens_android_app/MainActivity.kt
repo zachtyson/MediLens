a6c1e66e0868523839e980f866c7447241ffc106
chuerta018 <chuerta018@gmail.com>
2024-02-09 09:29:10 -0500

    A sign up page with username, email, and password

--- android-app/app/src/main/java/com/ztch/medilens_android_app/MainActivity.kt
+++ android-app/app/src/main/java/com/ztch/medilens_android_app/MainActivity.kt
@@ -16,7 +16,6 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
