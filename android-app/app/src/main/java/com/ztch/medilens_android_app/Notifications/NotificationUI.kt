2a5145c4d92ded1f13de869e166c5064dc6bbbb4
chuerta018 <chuerta018@gmail.com>
2024-03-07 23:17:19 -0500

    ui

---
+++ android-app/app/src/main/java/com/ztch/medilens_android_app/Notifications/NotificationUI.kt
@@ -0,0 +1,115 @@
+package com.ztch.medilens_android_app.Notifications
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.*
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowBackIosNew
+import androidx.compose.material3.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.ztch.medilens_android_app.R
+import com.ztch.medilens_android_app.appbarBottom
+
+
+import java.time.LocalDateTime
+import java.time.LocalTime
+
+@Preview(showSystemUi = true, device = "id:pixel_7_pro")
+@Composable
+fun notifcationPreview() {
+    notificationScreen(onNavigateToHomePage = {}, onNavigateToAlarmAdd = {})
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun notificationScreen(onNavigateToHomePage: () -> Unit,onNavigateToAlarmAdd : ()-> Unit) {
+
+
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = colorResource(R.color.DarkBlue),
+                    titleContentColor = Color.White
+                ),
+                title = {
+                    Text(
+                        "Reminders",
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                },
+                navigationIcon = {
+                    IconButton(onClick = { onNavigateToHomePage() }) {
+                        Icon(
+                            tint = Color.White,
+                            imageVector = Icons.Filled.ArrowBackIosNew,
+                            contentDescription = "Localized description"
+                        )
+                    }
+                },
+                actions = {
+                    IconButton(onClick = { onNavigateToAlarmAdd() }) {
+                        Icon(
+                            imageVector = Icons.Filled.Add,
+                            tint = Color.White,
+                            contentDescription = "addReminder"
+                        )
+                    }
+                },
+            )
+        },
+        containerColor = colorResource(R.color.DarkGrey),
+        content = { innerPadding ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding) // Use the padding provided by Scaffold for the content
+                    .background(color = colorResource(R.color.DarkGrey))
+            ) {
+                // Your content goes here. For example, if you want to display a list of items:
+                Card(
+                    colors = CardDefaults.cardColors(
+                        containerColor = colorResource(R.color.DarkestBlue)
+                    ),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(200.dp)
+                        .padding(16.dp)
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(16.dp)
+                    ) {
+                        Text(
+                            text = "No Notification set",
+                            fontSize = 24.sp,
+                            color = Color.White,
+                            fontWeight = FontWeight.Bold
+                        )
+
+                        // If NotificationDemo is a composable you want to display, include it here
+                        // NotificationDemo()
+                    }
+                }
+                // Add more components as needed
+            }
+
+        }
+    )
+
+}
