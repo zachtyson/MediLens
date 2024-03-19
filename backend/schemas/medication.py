bc3a82b2e9e2778a3b66e7ee123f73cc9d0b3e71
Zachary <tysonzachary@ufl.edu>
2024-03-19 19:38:13 -0400

    temporary medication schema/model

---
+++ backend/schemas/medication.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel
+from datetime import datetime
+from typing import Optional, List, Union
+
+# Temporary Medication class
+# Until Google Health API is implemented
+class Medication(BaseModel):
+    name: str
+    color: Optional[str]
+    imprint: Optional[str]
+    shape: Optional[str]
+    dosage: Optional[str]
+    intake_method: Optional[str]
+    description: Optional[str]
+    schedule_start: Optional[datetime]
+    interval_milliseconds: Optional[int]
+    owner_id: int
+
+    class Config:
+        orm_mode = True
