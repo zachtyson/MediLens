8870e5f055ad783e33ed123d61f42e06743629a5
Zachary <tysonzachary@ufl.edu>
2024-03-19 23:04:43 -0400

    db session fix

--- backend/db/session.py
+++ backend/db/session.py
@@ -1,9 +1,10 @@
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, MetaData
+from sqlalchemy import create_engine
 from databases import Database
 from core.config import settings
-from models.user import Base, User
+from models.user import User, Base as UserBase
+from models.medication import Medication, Base as MedicationBase
 
 # Create the engine
 engine = create_engine(settings.DATABASE_URL, pool_size=5, max_overflow=1)
@@ -14,11 +15,9 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 # Create a databases.Database instance
 database = Database(settings.DATABASE_URL)
 
-# Define the metadata
-metadata = MetaData()
-
-Base.metadata.create_all(bind=engine)
-
+# Create tables
+UserBase.metadata.create_all(bind=engine)
+MedicationBase.metadata.create_all(bind=engine)
 
 def get_db() -> Session:
     db = SessionLocal()
