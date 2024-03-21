8d9c8348f87845a15916819cd21ee5825026637d
Zachary <tysonzachary@ufl.edu>
2024-03-21 02:57:56 -0400

    api function to log in

---
+++ android-app/app/src/main/java/com/ztch/medilens_android_app/ApiUtils/ApiService.kt
@@ -0,0 +1,14 @@
+package com.ztch.medilens_android_app.ApiUtils
+import retrofit2.Call
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+interface ApiService {
+    @FormUrlEncoded
+    @POST("login/e/")
+    fun loginUser(
+        @Field("email") email: String,
+        @Field("password") password: String
+    ): Call<LoginTokenResponse> // TokenResponse is a data class you'll define to match the JSON response
+
+}
