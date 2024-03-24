6db33e331f65d15152bb0924f9086836da8a6c85
Zachary <tysonzachary@ufl.edu>
2024-03-24 15:43:45 -0400

    login auth to save and access tokens

---
+++ android-app/app/src/main/java/com/ztch/medilens_android_app/ApiUtils/LoginAuth.kt
@@ -0,0 +1,26 @@
+package com.ztch.medilens_android_app.ApiUtils
+
+import android.content.Context
+object LoginAuth {
+    fun isLoggedIn(context: Context): Boolean {
+        return hasToken(context, "access_token")
+    }
+
+    fun logIn(context: Context, token: String) {
+        saveToken(context, "access_token", token)
+    }
+
+    fun saveToken(context: Context, token_name: String, token: String) {
+        val sharedPref = context.getSharedPreferences("medilens", Context.MODE_PRIVATE)
+        with(sharedPref.edit()) {
+            putString(token_name, token)
+            apply()
+        }
+    }
+
+    fun hasToken(context: Context, token: String): Boolean {
+        // check if token exists
+        val sharedPref = context.getSharedPreferences("medilens", Context.MODE_PRIVATE)
+        return sharedPref.contains(token)
+    }
+}
