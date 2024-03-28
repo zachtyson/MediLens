6b5da25e9afb3ac9a994452e426a8cf3680b94c8
Zachary <tysonzachary@ufl.edu>
2024-03-28 02:06:21 -0400

    proof of concept pill finder

--- backend/api/pill_from_imprint/demo.py
+++ backend/api/pill_from_imprint/demo.py
@@ -1,3 +1,4 @@
+import requests
 import torch
 from PIL import Image
 import numpy as np
@@ -10,12 +11,54 @@ import pandas as pd
 import torch.nn as nn
 from ultralytics import YOLO
 import easyocr
+from bs4 import BeautifulSoup
 
 router = APIRouter()
 
 
-# Proof of concept for the pill_from_imprint endpoint, doesn't use an official API and uses web scraping
-# Will be replaced with a real API in the future
+# Proof of concept for the pill_from_imprint endpoint,
+# uses drugs.com endpoint which for some reason returns a html page
+# will be replaced later with a more reliable endpoint
+
 @router.get("/pill_from_imprint-demo")
 async def pill_from_imprint(imprint: str, color: int, shape: int):
-    return {"imprint": imprint, "color": color, "shape": shape}
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) Gecko/20100101 Firefox/124.0",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Sec-GPC": "1",
+        "Upgrade-Insecure-Requests": "1",
+    }
+    url = f"https://www.drugs.com/imprints.php?imprint={imprint}&color={color}&shape={shape}"
+    response = requests.get(url, headers=headers)
+    res = response.text
+
+    soup = BeautifulSoup(res, 'html.parser')
+
+    # Find all divs with the specific class
+    div_content = soup.find_all('div', class_='ddc-card')
+
+    # Initialize a dictionary to store the extracted information
+    extracted_info = []
+
+    # Extract and print the desired information
+    if div_content:
+        for div in div_content:
+            # Get URL: data-image-src
+            img_url = div.find('div')['data-image-src']
+            obj = {}
+            if img_url == '/img/pillid/no-image-placeholder.png':
+                img_url = None
+            obj['Image URL'] = img_url
+
+            div_smaller = div.find_all('div', class_='ddc-card-content ddc-card-content-pid')
+            for d in div_smaller:
+                dt_dd_pairs = d.find_all(['dt', 'dd'])
+                # extract the content of the <a> tag
+                obj['Pill Name'] = d.find('a').get_text().strip()
+                for i in range(0, len(dt_dd_pairs), 2):
+                    key = dt_dd_pairs[i].get_text().strip()
+                    value = dt_dd_pairs[i + 1].get_text().strip()
+                    obj[key] = value
+                extracted_info.append(obj)
+    return extracted_info
