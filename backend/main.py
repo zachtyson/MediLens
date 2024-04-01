47c2fc638ba4d85a8121bf35db677cb7bbca433a
Zachary <tysonzachary@ufl.edu>
2024-04-01 17:16:32 -0400

    remove old camera router

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from api.routes import camera
 import os
 
 # add routes from /api/routes
@@ -14,7 +13,6 @@ app.include_router(user_login_routes.router)
 app.include_router(user_registration_routes.router)
 app.include_router(ml_model_routes.router)
 app.include_router(medication_routes.router)
-app.include_router(camera.router)
 app.include_router(pill_from_imprint_demo.router)
 
 # gets the origins from the environment variable CORS_ORIGINS, if it exists, or defaults to the android studio emulator
