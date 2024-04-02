fe6b481c6b16fb8b67aae4d87f873a9f32c7c22f
Zachary <tysonzachary@ufl.edu>
2024-04-01 23:37:35 -0400

    created more registration tests

--- backend/tests/api/routes/test_registration.py
+++ backend/tests/api/routes/test_registration.py
@@ -61,8 +61,81 @@ async def test_create_user(async_client: AsyncClient, test_db: AsyncSession):
 
 
 @pytest.mark.asyncio
-async def test_get_users(async_client: AsyncClient, test_db: AsyncSession):
+async def test_create_user_duplicate(async_client: AsyncClient, test_db: AsyncSession):
+    email = "duplicate@email.com"
+    password = "password123"
+    user_data = {"email": email, "password": password}
 
+    # first verify that the user is not already in the database
+    async with test_db as db:
+        query = select(User).filter(User.email == email)
+        db_user = await db.execute(query)
+        db_user = db_user.scalars().first()
+        assert db_user is None
+
+    # create the user
+    response = await async_client.post("/users", json=user_data)
+    assert response.status_code == 200
+    user = response.json()
+    assert user["email"] == email
+
+    # try to create the user again
+    response = await async_client.post("/users", json=user_data)
+    assert response.status_code == 400
+    assert response.json() == {"detail": "Email already registered"}
+
+    # delete the user directly from the database
+    async with test_db as db:
+        query = select(User).filter(User.email == email)
+        db_user = await db.execute(query)
+        db_user = db_user.scalars().first()
+        await db.delete(db_user)
+        await db.commit()
+
+    # verify the user is actually deleted from the database
+    async with test_db as db:
+        query = select(User).filter(User.email == email)
+        db_user = await db.execute(query)
+        db_user = db_user.scalars().first()
+        assert db_user is None
+
+
+@pytest.mark.asyncio
+async def test_create_user_no_email(async_client: AsyncClient, test_db: AsyncSession):
+    user_data = {"password": "password123"}
+    response = await async_client.post("/users", json=user_data)
+    assert response.status_code == 422
+
+
+@pytest.mark.asyncio
+async def test_create_user_no_password(async_client: AsyncClient, test_db: AsyncSession):
+    user_data = {"email": "email@email.com"}
+    response = await async_client.post("/users", json=user_data)
+    assert response.status_code == 422
+
+
+@pytest.mark.asyncio
+async def test_create_user_no_email_password(async_client: AsyncClient, test_db: AsyncSession):
+    response = await async_client.post("/users")
+    assert response.status_code == 422
+
+
+@pytest.mark.asyncio
+async def test_create_user_invalid_email(async_client: AsyncClient, test_db: AsyncSession):
+    user_data = {"email": "invalidemail", "password": "password123"}
+    response = await async_client.post("/users", json=user_data)
+    assert response.status_code == 422
+
+
+@pytest.mark.asyncio
+async def test_get_users_empty(async_client: AsyncClient, test_db: AsyncSession):
+    response = await async_client.get("/users")
+    assert response.status_code == 200
+    assert response.json() == []
+
+
+@pytest.mark.asyncio
+async def test_get_users(async_client: AsyncClient, test_db: AsyncSession):
     # verify that user1 email, user2 email, and user3 email are not already in the database
     user_data1 = {"email": "user1@email.com", "password": "password123"}
     user_data2 = {"email": "user2@email.com", "password": "password123"}
@@ -139,7 +212,13 @@ async def test_get_user(async_client: AsyncClient, test_db: AsyncSession):
         await db.commit()
         await db.refresh(db_user)
 
-    response = await async_client.get(f"/users/{db_user.id}")
+    # get token from the login route
+    response = await async_client.post(f"/login/e",
+                                       data={"email": user_data["email"], "password": user_data["password"]})
+    assert response.status_code == 200
+    token = response.json()["access_token"]
+
+    response = await async_client.get(f"/users/{db_user.id}", headers={"token": f"{token}"})
     assert response.status_code == 200
     user = response.json()
     assert user["email"] == user_data["email"]
@@ -154,6 +233,69 @@ async def test_get_user(async_client: AsyncClient, test_db: AsyncSession):
         await db.delete(db_user)
         await db.commit()
 
+
+@pytest.mark.asyncio
+async def test_get_user_fake_token(async_client: AsyncClient, test_db: AsyncSession):
+    fake_token = "fake"
+    response = await async_client.get("/users/0", headers={"token": f"{fake_token}"})
+    assert response.status_code == 401
+    assert response.json() == {"detail": "Invalid token"}
+
+@pytest.mark.asyncio
+async def test_get_user_unauthorized(async_client: AsyncClient, test_db: AsyncSession):
+    # create the user
+    email = "email@email.com"
+    password = "password123"
+    user_data = {"email": email, "password": password}
+    async with test_db as db:
+        db_user = User(email=user_data["email"], hashed_password=get_password_hash(user_data["password"]))
+        db.add(db_user)
+        await db.commit()
+        await db.refresh(db_user)
+
+    # get new user id from the database
+    async with test_db as db:
+        query = select(User).filter(User.email == user_data["email"])
+        db_user = await db.execute(query)
+        db_user = db_user.scalars().first()
+        assert db_user is not None
+
+    id = db_user.id
+
+    # get token from the login route
+    response = await async_client.post(f"/login/e",
+                                        data={"email": user_data["email"], "password": user_data["password"]})
+    assert response.status_code == 200
+    token = response.json()["access_token"]
+
+    # try to get user with a different id
+
+    wrong_id = id + 1
+
+    response = await async_client.get(f"/users/{wrong_id}", headers={"token": f"{token}"})
+    assert response.status_code == 401
+    assert response.json() == {"detail": "Unauthorized"}
+
+    # delete the user directly from the database
+    async with test_db as db:
+        query = select(User).filter(User.email == user_data["email"])
+        db_user = await db.execute(query)
+        db_user = db_user.scalars().first()
+        await db.delete(db_user)
+        await db.commit()
+
+    # verify the user is actually deleted from the database
+    async with test_db as db:
+        query = select(User).filter(User.email == user_data["email"])
+        db_user = await db.execute(query)
+        db_user = db_user.scalars().first()
+        assert db_user is None
+
+
+@pytest.mark.asyncio
+async def test_get_user_not_found(async_client: AsyncClient, test_db: AsyncSession):
+    # todo: come back when the delete user route is implemented
+    assert True
 
 @pytest.mark.asyncio
 async def test_update_user(async_client: AsyncClient, test_db: AsyncSession):
@@ -172,25 +314,10 @@ async def test_update_user(async_client: AsyncClient, test_db: AsyncSession):
         db.add(db_user)
         await db.commit()
         await db.refresh(db_user)
-
-    """@router.post("/login/e", response_model=Token)
-async def create_token(email: Annotated[str, Form()], password: Annotated[str, Form()]):
-    user = authenticate_user_email(email, password, SessionLocal())
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect email or password",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    access_token_expires = timedelta(minutes=access_token_minutes)
-    # subject = email and user.id in json format
-    sub = {"email": user.email, "id": user.id}
-    access_token = create_access_token(sub, expires_delta=access_token_expires)
-    return {"access_token": access_token, "token_type": "bearer"}"""
     # testing this function DOES require the creation of a token, so this can't be tested in isolation
     # email and password in request body
     response = await async_client.post(f"/login/e",
-                                      data={"email": user_data["email"], "password": user_data["password"]})
+                                       data={"email": user_data["email"], "password": user_data["password"]})
     assert response.status_code == 200
     # get token from access_token
     token = response.json()["access_token"]
@@ -218,5 +345,4 @@ async def create_token(email: Annotated[str, Form()], password: Annotated[str, F
         db_user = db_user.scalars().first()
         await db.delete(db_user)
         await db.commit()
-
 
