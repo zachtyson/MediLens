1d97ca7135c6673f71b1ef851b1e4d911b3edb73
Zachary <tysonzachary@ufl.edu>
2024-04-05 01:28:12 -0400

    working change password

--- android-app/app/src/main/java/com/ztch/medilens_android_app/Settings/Settings.kt
+++ android-app/app/src/main/java/com/ztch/medilens_android_app/Settings/Settings.kt
@@ -55,6 +55,9 @@ fun Settings (
     var showChangePasswordDialog by remember { mutableStateOf(false) }
     var showLogOutDialog by remember { mutableStateOf(false) }
 
+    var oldPasswordChange by remember { mutableStateOf("") }
+    var newPasswordChange by remember { mutableStateOf("") }
+
 
     if (showChangeEmailDialog) {
         ChangeEmailDialog(onDismiss = { showChangeEmailDialog = false })
@@ -63,6 +66,7 @@ fun Settings (
     if (showDeleteAccountDialog) {
         ConfirmDeleteAccountDialog(
             onDismiss = { showDeleteAccountDialog = false },
+
             onDeleteConfirmed = {
                 // Handle account deletion here
                 showDeleteAccountDialog = false
@@ -74,10 +78,23 @@ fun Settings (
         )
     }
 
+
+    var onPasswordUpdateSuccess by remember { mutableStateOf<Boolean?>(null) }
+    var onPasswordUpdateFailure by remember { mutableStateOf<Boolean?>(null) }
+
     if (showChangePasswordDialog) {
         ChangePasswordDialog(
             onDismiss = { showChangePasswordDialog = false },
+            context = context,
+            onUpdateFailure = { onPasswordUpdateFailure = null },
+            onUpdateSuccess = { onPasswordUpdateSuccess = null }
         )
+    }
+
+    if (onPasswordUpdateSuccess != null) {
+        Toast.makeText(context, "Password updated", Toast.LENGTH_SHORT).show()
+    } else if (onPasswordUpdateFailure != null) {
+        Toast.makeText(context, "Failed to update password", Toast.LENGTH_SHORT).show()
     }
 
     if (showLogOutDialog) {
@@ -165,6 +182,91 @@ fun Settings (
     )
 }
 
+//private fun fetchMedications(
+//    service: ApiService,
+//    context: Context,
+//    medications: MutableState<List<Medication>>
+//) {
+//    service.getMedications(TokenAuth.getLogInToken(context)).enqueue(object : Callback<List<Medication>> {
+//        override fun onResponse(call: Call<List<Medication>>, response: Response<List<Medication>>) {
+//            if (response.isSuccessful) {
+//                medications.value = response.body() ?: emptyList()
+//            } else {
+//                Log.e("Cabinet", "Failed to fetch medications")
+//            }
+//        }
+//
+//        override fun onFailure(call: Call<List<Medication>>, t: Throwable) {
+//            Log.e("Cabinet", "Failed to fetch medications", t)
+//        }
+//    })
+//}
+
+private fun updateUserEmail(
+    service: ApiService,
+    context: Context,
+    email: String,
+    password: String
+) {
+    service.updateEmail(TokenAuth.getLogInToken(context), email, password).enqueue(object : Callback<RegisterResponse> {
+        override fun onResponse(call: Call<RegisterResponse>, response: Response<RegisterResponse>) {
+            if (response.isSuccessful) {
+                Log.d("Settings", "Email updated")
+            } else {
+                Log.e("Settings", "Failed to update email")
+            }
+        }
+
+        override fun onFailure(call: Call<RegisterResponse>, t: Throwable) {
+            Log.e("Settings", "Failed to update email", t)
+        }
+    })
+}
+
+private fun updateUserPassword(
+    service: ApiService,
+    context: Context,
+    oldPassword: String,
+    newPassword: String,
+    onSuccess: () -> Unit,
+    onFailure: (String) -> Unit
+) {
+    service.updatePassword(TokenAuth.getLogInToken(context), oldPassword, newPassword).enqueue(object : Callback<RegisterResponse> {
+        override fun onResponse(call: Call<RegisterResponse>, response: Response<RegisterResponse>) {
+            if (response.isSuccessful) {
+                Log.d("Settings", "Password updated")
+            } else {
+                Log.e("Settings", "Failed to update password")
+            }
+        }
+
+        override fun onFailure(call: Call<RegisterResponse>, t: Throwable) {
+            Log.e("Settings", "Failed to update password", t)
+        }
+    })
+}
+
+private fun deleteUserAccount(
+    service: ApiService,
+    context: Context,
+    password: String
+) {
+    service.deleteUser(TokenAuth.getLogInToken(context), password).enqueue(object : Callback<Map<String, String>> {
+        override fun onResponse(call: Call<Map<String, String>>, response: Response<Map<String, String>>) {
+            if (response.isSuccessful) {
+                Log.d("Settings", "Account deleted")
+            } else {
+                Log.e("Settings", "Failed to delete account")
+            }
+        }
+
+        override fun onFailure(call: Call<Map<String, String>>, t: Throwable) {
+            Log.e("Settings", "Failed to delete account", t)
+        }
+    })
+}
+
+
 @Composable
 fun SettingsOptionItem(
     icon: ImageVector,
@@ -204,7 +306,12 @@ fun ChangeEmailDialog(onDismiss: () -> Unit) {
                 OutlinedTextField(
                     value = newEmail,
                     onValueChange = { newEmail = it },
-                    label = { Text("Email") }
+                    label = { Text("Old Email") }
+                )
+                OutlinedTextField(
+                    value = newEmail,
+                    onValueChange = { newEmail = it },
+                    label = { Text("New Email") }
                 )
                 OutlinedTextField(
                     value = confirmEmail,
@@ -244,10 +351,16 @@ fun ChangeEmailDialog(onDismiss: () -> Unit) {
 }
 
 @Composable
-fun ChangePasswordDialog(onDismiss: () -> Unit) {
+fun ChangePasswordDialog(onDismiss: () -> Unit,
+                         context: Context,
+                         onUpdateSuccess: () -> Unit = {},
+                         onUpdateFailure: (String) -> Unit = {}
+) {
+
+    var oldPassword by remember { mutableStateOf("") }
     var newPassword by remember { mutableStateOf("") }
     var confirmPassword by remember { mutableStateOf("") }
-    var error by remember { mutableStateOf(false) }
+    var error by remember { mutableStateOf("") }
 
     AlertDialog(
         onDismissRequest = { onDismiss() },
@@ -257,27 +370,41 @@ fun ChangePasswordDialog(onDismiss: () -> Unit) {
                 Text("Enter your new password:")
                 Spacer(modifier = Modifier.height(8.dp))
                 OutlinedTextField(
+                    value = oldPassword,
+                    onValueChange = { oldPassword = it },
+                    label = { Text("Old Password") }
+                )
+                OutlinedTextField(
                     value = newPassword,
                     onValueChange = { newPassword = it },
-                    label = { Text("Password") }
+                    label = { Text("New Password") }
                 )
                 OutlinedTextField(
                     value = confirmPassword,
                     onValueChange = { confirmPassword = it },
-                    label = { Text("Confirm Password") }
+                    label = { Text("Confirm New Password") }
                 )
-                if (error) {
-                    Text("Passwords do not match", color = Color.Red)
+                if (error != "") {
+                    Text(error, color = Color.Red)
                 }
             }
         },
         confirmButton = {
             TextButton(onClick = {
                 if (newPassword != confirmPassword) {
-                    error = true
+                    error = "Passwords do not match"
                     return@TextButton
                 }
-                onDismiss()
+                error = ""
+                // Make password change API call, dismiss if successful
+                // Otherwise if API call fails, set error message to 'Failed to change password'
+                updateUserPassword(RetrofitClient.apiService, context, oldPassword, confirmPassword, {
+                    onUpdateSuccess()
+                    onDismiss()
+                }, {
+                    error = it
+                    onUpdateFailure(it)
+                })
             }) {
                 Text("Confirm")
             }
