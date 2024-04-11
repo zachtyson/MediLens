94c85fa8f6a7244fd7386b7c4ec3b0175b73c982
Zachary <tysonzachary@ufl.edu>
2024-04-11 00:59:13 -0400

    fix add medication

--- backend/api/healthcare_info/medication_routes.py
+++ backend/api/healthcare_info/medication_routes.py
@@ -1,18 +1,17 @@
-from datetime import datetime
-
-from db.session import SessionLocal
-from fastapi import APIRouter, HTTPException, Form, Depends
-from models.user import User
-from models.medication import Medication
 from zoneinfo import ZoneInfo
-from core.security import get_token_from_header
 
-from schemas.medication import MedicationCreate
-from typing import Annotated, List
-
+from fastapi import APIRouter, Depends, HTTPException, Form
+from sqlalchemy import null
 from sqlalchemy.orm import Session
-
-from core.security import verify_token, get_id_from_token
+from models.user import User
+from models.base import Base
+from models.medication import Medication
+from db.session import SessionLocal
+from schemas.medication import MedicationCreate
+from schemas.user import UserCreate, UserUpdate, UserResponse
+from typing import List, Annotated
+from core.security import get_password_hash, get_token_from_header, get_id_from_token, verify_password, verify_token
+from fastapi.responses import JSONResponse
 
 router = APIRouter()
 
@@ -27,12 +26,9 @@ def get_db():
 
 # Simple route for adding medicine to the database, should return simple success message or error message
 @router.post("/medication/add_medication")
-async def add_medication(name: str = Form(...), color: str = Form(...),
-                         imprint: str = Form(...),
-                         shape: str = Form(...), dosage: str = Form(...), intake_method: str = Form(...),
-                         description: str = Form(...), schedule_start: str = Form(...),
-                         interval_milliseconds: int = Form(...), db: Session = Depends(get_db),
+async def add_medication(medication_data: MedicationCreate, db: Session = Depends(get_db),
                          token: str = Depends(get_token_from_header)):
+
     if not verify_token(token):
         raise HTTPException(status_code=401, detail="Invalid token")
     # get user id from token
@@ -41,29 +37,24 @@ async def add_medication(name: str = Form(...), color: str = Form(...),
     # store schedule_start as a datetime object with timezone, might be needed later
     # schedule_start = datetime.fromisoformat(schedule_start)
     # schedule_start = schedule_start.replace(tzinfo=ZoneInfo("UTC"))
-
     print(user_id)
     if not db.query(User).filter(User.id == user_id).first():
         raise HTTPException(status_code=404, detail="User not found")
     try:
-        medication_info = MedicationCreate(name=name, color=color, imprint=imprint, shape=shape, dosage=dosage,
-                                           intake_method=intake_method, description=description,
-                                           schedule_start=schedule_start,
-                                           interval_milliseconds=interval_milliseconds)
-        new_name = medication_info.name
-        new_color = medication_info.color
-        new_imprint = medication_info.imprint
-        new_shape = medication_info.shape
-        new_dosage = medication_info.dosage
-        new_intake_method = medication_info.intake_method
-        new_description = medication_info.description
-        new_schedule_start = medication_info.schedule_start
-        new_interval_milliseconds = medication_info.interval_milliseconds
-        new_medication = Medication(name=new_name, color=new_color, imprint=new_imprint,
-                                    shape=new_shape, dosage=new_dosage, intake_method=new_intake_method,
-                                    description=new_description, schedule_start=new_schedule_start,
-                                    interval_milliseconds=new_interval_milliseconds, owner_id=user_id) \
-            # Add the new medication to the database
+        # get all the data from the request
+        new_medication = Medication(
+            owner_id=user_id,
+            name=medication_data.name,
+            description=medication_data.description,
+            color=medication_data.color,
+            imprint=medication_data.imprint,
+            shape=medication_data.shape,
+            dosage=medication_data.dosage,
+            intake_method=medication_data.intake_method,
+            init_vector=medication_data.init_vector
+        )
+
+        # Add the new medication to the database
         db.add(new_medication)
         db.commit()
         db.refresh(new_medication)
@@ -79,19 +70,17 @@ async def get_medications(db: Session = Depends(get_db), token: str = Depends(ge
     if not verify_token(token):
         raise HTTPException(status_code=401, detail="Invalid token")
     user_id = get_id_from_token(token)
-    print(user_id)
     if not db.query(User).filter(User.id == user_id).first():
         raise HTTPException(status_code=404, detail="User not found")
     medications = db.query(Medication).filter(Medication.owner_id == user_id).all()
     # convert created_date to ISO8601 format with timezone
 
     for med in medications:
-        if med.created_date:  # Check if created_date is not None
+        if med.created_date is not None:
             med.created_date = med.created_date.replace(tzinfo=ZoneInfo("UTC")).isoformat()
-        if med.schedule_start:  # Check if schedule_start is not None
+        if med.schedule_start is not None:
             med.schedule_start = med.schedule_start.replace(tzinfo=ZoneInfo("UTC")).isoformat()
 
     # convert to dictionary
     medications = [med.__dict__ for med in medications]
-    print(medications)
     return medications
