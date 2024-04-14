138d1036d541ccb004f183d47c1ed51633ff18f3
Zachary <tysonzachary@ufl.edu>
2024-04-14 04:56:44 -0400

    select start date and interval

--- android-app/app/src/main/java/com/ztch/medilens_android_app/Notifications/ScheduleMedication.kt
+++ android-app/app/src/main/java/com/ztch/medilens_android_app/Notifications/ScheduleMedication.kt
@@ -1,10 +1,10 @@
 package com.ztch.medilens_android_app.Notifications
 
 
+import android.app.TimePickerDialog
 import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBackIosNew
 import androidx.compose.material3.*
@@ -21,23 +21,11 @@ import com.ztch.medilens_android_app.Authenticate.encryptData
 import com.ztch.medilens_android_app.Authenticate.getLocalEncryptionKey
 import com.ztch.medilens_android_app.R
 import com.ztch.medilens_android_app.Refill.SharedMedicationModel
-import androidx.compose.runtime.*
-import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.material.*
-import androidx.compose.ui.platform.LocalContext
 import java.util.*
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.icons.filled.ArrowBackIosNew
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.unit.dp
-import com.ztch.medilens_android_app.ApiUtils.*
-import com.ztch.medilens_android_app.Authenticate.createRandomIV
-import com.ztch.medilens_android_app.Authenticate.encryptData
-import java.util.*
+import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ScheduleMedication (
@@ -46,7 +34,9 @@ fun ScheduleMedication (
     onNavigateToCabinet: () -> Unit,
     sharedMedicationModel: SharedMedicationModel
 ) {
-    // Add Medication
+    val snackState = remember { SnackbarHostState() }
+    val snackScope = rememberCoroutineScope()
+    SnackbarHost(hostState = snackState, Modifier)
 
     val service = RetrofitClient.apiService
     Log.d("ModifyMedication", "Recomposed")
@@ -64,7 +54,136 @@ fun ScheduleMedication (
     var medicationScheduleInterval by remember { mutableStateOf(medication.interval_milliseconds) }
     var medicationScheduleIntervalTimePicker by remember { mutableStateOf(IntervalSchedule(0, 0, 0, 0)) }
     var errorText = remember { mutableStateOf("") }
-    var showDatePicker by remember { mutableStateOf(false) }
+
+    val openDatePicker = remember { mutableStateOf(false) }
+    val openTimePicker = remember { mutableStateOf(false) }
+    val openIntervalPicker = remember { mutableStateOf(false) }
+    if (openDatePicker.value) {
+        val datePickerState = rememberDatePickerState()
+        val confirmEnabled = remember {
+            derivedStateOf { datePickerState.selectedDateMillis != null }
+        }
+        DatePickerDialog(
+            onDismissRequest = {
+                // Dismiss the dialog when the user clicks outside the dialog or on the back
+                // button. If you want to disable that functionality, simply use an empty
+                // onDismissRequest.
+                openDatePicker.value = false
+            },
+            confirmButton = {
+                TextButton(
+                    onClick = {
+                        openDatePicker.value = false
+                        snackScope.launch {
+                            snackState.showSnackbar(
+                                "Selected date timestamp: ${datePickerState.selectedDateMillis}"
+                            )
+                            val newDate = Date(datePickerState.selectedDateMillis!!)
+                            medicationScheduleStartDate = newDate
+                            Log.d("DatePicker", "Selected date: $newDate")
+                        }
+                        // open time picker
+                        openTimePicker.value = true
+
+                    },
+                    enabled = confirmEnabled.value
+                ) {
+                    Text("OK")
+                }
+            },
+            dismissButton = {
+                TextButton(
+                    onClick = {
+                        openDatePicker.value = false
+                    }
+                ) {
+                    Text("Cancel")
+                }
+            }
+        ) {
+            DatePicker(state = datePickerState)
+        }
+    }
+    if (openTimePicker.value) {
+        TimePickerDialog(
+            context,
+            { _, hour, minute ->
+                val calendar = Calendar.getInstance().apply {
+                    set(Calendar.HOUR_OF_DAY, hour)
+                    set(Calendar.MINUTE, minute)
+                }
+                // Change the time of the medication schedule start date but not the date
+                val newDate = medicationScheduleStartDate?.let {
+                    val newCalendar = Calendar.getInstance().apply {
+                        time = it
+                        set(Calendar.HOUR_OF_DAY, hour)
+                        set(Calendar.MINUTE, minute)
+                    }
+                    newCalendar.time
+                }
+                medicationScheduleStartDate = newDate
+                openTimePicker.value = false
+            },
+            12,
+            0,
+            false
+        ).show()
+    }
+    if (openIntervalPicker.value) {
+        val intervalPickerState = remember {
+            mutableStateOf(IntervalSchedule(0, 0, 0, 0))
+        }
+        val confirmEnabled = remember {
+            derivedStateOf {
+                intervalPickerState.value.weeks > 0 ||
+                        intervalPickerState.value.days > 0 ||
+                        intervalPickerState.value.hours > 0 ||
+                        intervalPickerState.value.minutes > 0
+            }
+        }
+        AlertDialog(
+            onDismissRequest = {
+                openIntervalPicker.value = false
+            },
+            title = {
+                Log.d("openDatePicker", openDatePicker.value.toString())
+                Log.d("openTimePicker", openTimePicker.value.toString())
+                Log.d("openIntervalPicker", openIntervalPicker.value.toString())
+                Text("Select Interval")
+            },
+            text = {
+                Column {
+                    IntervalPicker(
+                        state = intervalPickerState,
+                        modifier = Modifier.padding(8.dp)
+                    )
+                }
+            },
+            confirmButton = {
+                TextButton(
+                    onClick = {
+                        openIntervalPicker.value = false
+                        medicationScheduleIntervalTimePicker = intervalPickerState.value
+                        medicationScheduleInterval = intervalPickerState.value.toMilliseconds()
+                        Log.d("IntervalPicker", "Selected interval: $medicationScheduleInterval")
+                    },
+                    enabled = confirmEnabled.value
+                ) {
+                    Text("OK")
+                }
+            },
+            dismissButton = {
+                TextButton(
+                    onClick = {
+                        openIntervalPicker.value = false
+                    }
+                ) {
+                    Text("Cancel")
+                }
+            }
+        )
+    }
+
     Scaffold(
         topBar = {
             CenterAlignedTopAppBar(
@@ -105,103 +224,71 @@ fun ScheduleMedication (
                         .padding(16.dp),
                     verticalArrangement = Arrangement.Center
                 ) {
-                    // Unmodifiable text fields for the rest of the medication details
-                    OutlinedTextField(
-                        colors = TextFieldDefaults.colors(
-                            unfocusedContainerColor = colorResource(id = R.color.DarkBlue),
-                            focusedContainerColor = colorResource(id = R.color.DarkBlue),
-                            focusedTextColor = Color.White,
-                            unfocusedTextColor = Color.White
+                    // Card with medication information
+                    Card( colors = CardDefaults.cardColors(
+                        containerColor = colorResource(R.color.DarkBlue),
+                        contentColor = colorResource(R.color.DarkBlue),
+
                         ),
-                        value = "Name: ${medication.name}",
-                        onValueChange = { },
-                        readOnly = true,
-                        modifier = Modifier.fillMaxWidth(),
-                    )
-                    OutlinedTextField(
-                        colors = TextFieldDefaults.colors(
-                            unfocusedContainerColor = colorResource(id = R.color.DarkBlue),
-                            focusedContainerColor = colorResource(id = R.color.DarkBlue),
-                            focusedTextColor = Color.White,
-                            unfocusedTextColor = Color.White
-                        ),
-                        value = "Description: ${medication.description ?: "N/A"}",
-                        onValueChange = { },
-                        readOnly = true,
-                        modifier = Modifier.fillMaxWidth(),
-                    )
-                    OutlinedTextField(
-                        colors = TextFieldDefaults.colors(
-                            unfocusedContainerColor = colorResource(id = R.color.DarkBlue),
-                            focusedContainerColor = colorResource(id = R.color.DarkBlue),
-                            focusedTextColor = Color.White,
-                            unfocusedTextColor = Color.White
-                        ),
-                        value = "Color: ${medication.color ?: "N/A"}",
-                        onValueChange = { },
-                        readOnly = true,
-                        modifier = Modifier.fillMaxWidth(),
-                    )
-                    OutlinedTextField(
-                        colors = TextFieldDefaults.colors(
-                            unfocusedContainerColor = colorResource(id = R.color.DarkBlue),
-                            focusedContainerColor = colorResource(id = R.color.DarkBlue),
-                            focusedTextColor = Color.White,
-                            unfocusedTextColor = Color.White
-                        ),
-                        value = "Imprint: ${medication.imprint ?: "N/A"}",
-                        onValueChange = { },
-                        readOnly = true,
-                        modifier = Modifier.fillMaxWidth(),
-                    )
-                    OutlinedTextField(
-                        colors = TextFieldDefaults.colors(
-                            unfocusedContainerColor = colorResource(id = R.color.DarkBlue),
-                            focusedContainerColor = colorResource(id = R.color.DarkBlue),
-                            focusedTextColor = Color.White,
-                            unfocusedTextColor = Color.White
-                        ),
-                        value = "Shape: ${medication.shape ?: "N/A"}",
-                        onValueChange = { },
-                        readOnly = true,
-                        modifier = Modifier.fillMaxWidth(),
-                    )
-                    OutlinedTextField(
-                        colors = TextFieldDefaults.colors(
-                            unfocusedContainerColor = colorResource(id = R.color.DarkBlue),
-                            focusedContainerColor = colorResource(id = R.color.DarkBlue),
-                            focusedTextColor = Color.White,
-                            unfocusedTextColor = Color.White
-                        ),
-                        value = "Dosage: ${medication.dosage ?: "N/A"}",
-                        onValueChange = { },
-                        readOnly = true,
-                        modifier = Modifier.fillMaxWidth(),
-                    )
-                    OutlinedTextField(
-                        colors = TextFieldDefaults.colors(
-                            unfocusedContainerColor = colorResource(id = R.color.DarkBlue),
-                            focusedContainerColor = colorResource(id = R.color.DarkBlue),
-                            focusedTextColor = Color.White,
-                            unfocusedTextColor = Color.White
-                        ),
-                        value = "Intake Method: ${medication.intake_method ?: "N/A"}",
-                        onValueChange = { },
-                        readOnly = true,
-                        modifier = Modifier.fillMaxWidth(),
-                    )
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            // take up as much height as needed
+                            .height(IntrinsicSize.Min)
+                            .padding(16.dp)
+                    ) {
+                        InfoField(
+                            label = "Name",
+                            value = medication.name
+                        )
+                        InfoField(
+                            label = "Description",
+                            value = medication.description
+                        )
+                        InfoField(
+                            label = "Color",
+                            value = medication.color
+                        )
+                        InfoField(
+                            label = "Imprint",
+                            value = medication.imprint
+                        )
+                        InfoField(
+                            label = "Shape",
+                            value = medication.shape
+                        )
+                        InfoField(
+                            label = "Dosage",
+                            value = medication.dosage
+                        )
+                        InfoField(
+                            label = "Intake Method",
+                            value = medication.intake_method
+                        )
+                    }
                     Spacer(modifier = Modifier.height(16.dp))
                     // let user pick a start date using a date picker
                     //todo implement datepicker/timepicker
                     Button(
-                        onClick = { showDatePicker = true },
+                        onClick = {
+                            openDatePicker.value = true
+                        },
                         modifier = Modifier.padding(top = 8.dp).fillMaxWidth()
                     ) {
-                        Text(text = "Select Start Date")
+                        Text(text = "Select Start Date And Time", color = Color.White)
                     }
-
                     medicationScheduleStartDate?.let {
-                        Text("Start Date: ${formatDateTime(convertToLocalDateTime(it))}")
+                        Text(text = "Start Date: ${formatDateTime(convertToLocalDateTime(it))}", color = Color.White)
+                    }
+                    if (medicationScheduleStartDate != null) {
+                        Button(
+                            onClick = {
+                                openIntervalPicker.value = true
+                            },
+                            modifier = Modifier.padding(top = 8.dp).fillMaxWidth()
+                        ) {
+                            Text(text = "Select Interval", color = Color.White)
+                        }
+
                     }
                     Button(
                         onClick = {
@@ -255,7 +342,7 @@ fun ScheduleMedication (
                         modifier = Modifier.padding(top = 8.dp)
                             .fillMaxWidth()
                     ) {
-                        Text(text = "Modify Medication", color = Color.White)
+                        Text(text = "Submit Schedule", color = Color.White)
                     }
                 }
             }
@@ -273,5 +360,66 @@ data class IntervalSchedule(
 ) {
     fun toMilliseconds(): Long {
         return (weeks * 604800000L) + (days * 86400000L) + (hours * 3600000L) + (minutes * 60000L)
+    }
+}
+@Composable
+fun InfoField(
+    label: String,
+    value: String?,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.fillMaxWidth()
+    ) {
+        Text(
+            text = "$label: ${value ?: "N/A"}",
+            color = Color.White
+        )
+    }
+}
+
+// Lets user pick an interval in weeks, days, hours, and minutes
+// Simple text boxes that only allow numbers
+// or a checkbox for one-time medication
+@Composable
+fun IntervalPicker (
+    state: MutableState<IntervalSchedule>,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.fillMaxWidth()
+    ) {
+        TextField(
+            value = state.value.weeks.toString(),
+            onValueChange = {
+                state.value = state.value.copy(weeks = it.toIntOrNull() ?: 0)
+            },
+            label = { Text("Weeks") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        TextField(
+            value = state.value.days.toString(),
+            onValueChange = {
+                state.value = state.value.copy(days = it.toIntOrNull() ?: 0)
+            },
+            label = { Text("Days") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        TextField(
+            value = state.value.hours.toString(),
+            onValueChange = {
+                state.value = state.value.copy(hours = it.toIntOrNull() ?: 0)
+            },
+            label = { Text("Hours") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        TextField(
+            value = state.value.minutes.toString(),
+            onValueChange = {
+                state.value = state.value.copy(minutes = it.toIntOrNull() ?: 0)
+            },
+            label = { Text("Minutes") },
+            modifier = Modifier.fillMaxWidth()
+        )
     }
 }
